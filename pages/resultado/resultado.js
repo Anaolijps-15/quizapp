239260193518b07b54b4f8d24020128d1de9fb84
Anaolijps-15 oliveira <anaoli13jps@gmail.com>
2024-05-28 02:57:38 +0000

    funcao de resultado

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -3,9 +3,37 @@ import { verificarTema, trocarTema  } from "../../helpers/tema-helpers"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
+const assunto = localStorage.getItem("assunto")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
 verificarTema(body, botaoTema)
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+    divIcone.classList(assunto.toLocaleLowerCase)
+    iconeImg.setAttribute("scr", `../../assets/images/icon-${assunto.toLocaleLowerCase()}.svg`)
+    iconeImg.setAttribute('alt', `icone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
+
+function inserirResultado() {
+    const sectionPontuacao = document.querySelector (".pontuacao")
+    const divAssunto = document.querySelector("assunto")
+    const pontos = localStorage.getItem("pontos")
+
+    sectionPontuacao.innerHTML = 
+    `
+        ${divAssunto.outerHTML}
+        <strong>${pontos}</strong>
+        <p>de 10</p>
+    `
+}
+
+inserirResultado()
